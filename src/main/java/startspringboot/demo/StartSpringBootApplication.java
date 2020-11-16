e0ca7ca0385256a0649ab885baebba63f57f862c
Marina <maryna.voloshyna.11@gmail.com>
2020-11-17 00:09:08 +0200

    first commit

---
+++ src/main/java/startspringboot/demo/StartSpringBootApplication.java
@@ -0,0 +1,13 @@
+package startspringboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringBootApplication.class, args);
+    }
+
+}
