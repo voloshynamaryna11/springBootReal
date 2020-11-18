5d6e954eb04135bdf2ace47b395c6f5aa4bbae7f
Marina <maryna.voloshyna.11@gmail.com>
2020-11-18 14:03:01 +0200

    try with resources

--- src/main/java/startspringboot/demo/service/impl/FileReaderServiceImpl.java
+++ src/main/java/startspringboot/demo/service/impl/FileReaderServiceImpl.java
@@ -13,10 +13,8 @@ import java.util.stream.Collectors;
 public class FileReaderServiceImpl implements FileReaderService {
     @Override
     public List<List<String>> read(String path) {
-        CSVReader reader;
         List<List<String>> list = new ArrayList<>();
-        try {
-            reader = new CSVReader(new FileReader(path));
+        try(CSVReader reader = new CSVReader(new FileReader(path))) {
             String[] line;
             while ((line = reader.readNext()) != null) {
                 list.add(Arrays.stream(line).collect(Collectors.toList()));
